fb2f13f59a66135c88560a63311a785f0afa6cc0
Clayton Silva dos Santos <95229093+ClaytonSdS@users.noreply.github.com>
2025-08-20 12:33:59 +0000

    var commit

---
+++ Introdução à Programação de Computadores/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 0;
+    int qtd = 0; // Quantidade
+    char letra = 'a';
+    char nome[20] = 'aa';
+    float altura = 1.70;
+    double peso = 50; // Double -> armazena mais valores de ponto flutuante, quando comparada com tipo 'float'
+
+    
+
+}
