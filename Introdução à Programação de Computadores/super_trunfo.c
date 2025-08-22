6399fbba97f10c38fb6b1fd42ba49e0e3c8090b7
Clayton Silva dos Santos <95229093+ClaytonSdS@users.noreply.github.com>
2025-08-22 18:31:13 +0000

    update: super trunfo avançado

--- Introdução à Programação de Computadores/super_trunfo.c
+++ Introdução à Programação de Computadores/super_trunfo.c
@@ -13,6 +13,10 @@ int main(){
     float densidade_p1, densidade_p2;                // Densidade Populacional
     float pib_capita1, pib_capita2;                  // PIB per Capita
 
+    float super_pwr1, super_pwr2;                   // SuperPoder da Carta
+
+    unsigned int comp_populacao, comp_area, comp_pib, comp_turistico, comp_densidade, comp_pib_per_capita, comp_super_power // Variaveis para comparação
+
     // Código Para Solicitar as Informações da Primeira Carta (CARTA 1)
     printf("Informações para o preenchimento da CARTA 1:\n");
     printf("Caracter para o Estado (apenas 1 letra): ");
@@ -50,7 +54,7 @@ int main(){
     printf("População: ");
     scanf("%i", &populacao2);
 
-    printf("Área em km²: ");
+    printf("Área em (km²): ");
     scanf("%f", &area2);
 
     printf("PIB (em bilhões de reais): ");
@@ -71,12 +75,39 @@ int main(){
     pib_capita1 = (float) pib1 / populacao1;       // PIB per Capita da Cidade 1
     pib_capita2 = (float) pib2 / populacao2;       // PIB per Capita da Cidade 2
 
-    // Print com as Informações da Carta 1
-    printf("\nCarta 1: \nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %i\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %i\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f bilhões R$/hab\n", 
-                      estado1, codigo_completo1, cidade1, populacao1, area1, pib1, pontos_turismo1, densidade_p1, pib_capita1);
+    // Calcular Super Poder das Cartas
+    super_pwr1 = (float) populacao1 + area1 + pib1 + pib_capita1 + (1/densidade_p1) + pontos_turismo1;
+    super_pwr2 = (float) populacao2 + area2 + pib2 + pib_capita2 + (1/densidade_p2) + pontos_turismo1;
 
-    // Print com as Informações da Carta 2
-    printf("\nCarta 2: \nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %i\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %i\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f bilhões R$/hab\n", 
-                      estado2, codigo_completo2, cidade2, populacao2, area2, pib2, pontos_turismo2, densidade_p2, pib_capita2);
+    // Comparação entre as propriedades das cartas
+    comp_populacao = populacao1 > populacao2;
+    comp_area = area1 > area2;
+    comp_pib = pib1 > pib2;
+    comp_turistico = pontos_turismo1 > pontos_turismo2;
+    comp_densidade = densidade_p1 < densidade_p2; // menor densidade vence
+    comp_pib_per_capita = pib_capita1 > pib_capita2;
+    comp_super_power = super_pwr1 > super_pwr2;
 
-}
+    // Printando os resultados
+    printf("Comparação de Cartas:\n");
+
+    // Comparação População
+    if (comp_populacao) {printf("População: Carta 1 venceu (%u)\n", comp_populacao);} else {printf("População: Carta 2 venceu (%u)\n", comp_populacao);}
+
+    // Comparação Área
+    if (comp_area) {printf("nÁrea: Carta 1 venceu (%u)\n", comp_area);} else {printf("nÁrea: Carta 2 venceu (%u)\n", comp_area);}
+
+    // Comparação PIB
+    if (comp_pib) {printf("PIB: Carta 1 venceu (%u)\n", comp_pib);} else {printf("PIB: Carta 2 venceu (%u)\n", comp_pib);}
+
+    // Comparação Pontos Turísticos
+    if (comp_turistico) {printf("Pontos Turísticos: Carta 1 venceu (%u)\n", comp_turistico);} else {printf("Pontos Turísticos: Carta 2 venceu (%u)\n", comp_turistico);}
+    
+    // Comparação Densidade Populacional
+    if (comp_densidade) {printf("Densidade Populacional: Carta 1 venceu (%u)\n", comp_densidade);} else {printf("Densidade Populacional: Carta 2 venceu (%u)\n", comp_densidade);}
+       
+    // Comparação PIB per Capita
+    if (comp_pib_per_capita) {printf("PIB per Capita: Carta 1 venceu (%u)\n", comp_pib_per_capita);} else {printf("PIB per Capita: Carta 2 venceu (%u)\n", comp_pib_per_capita);}
+        
+    // Comparação Super Poder
+    if (comp_super_power) {printf("Super Poder: Carta 1 venceu (%u)\n", comp_super_power);} else {printf("Super Poder: Carta 2 venceu (%u)\n", comp_super_power);}
